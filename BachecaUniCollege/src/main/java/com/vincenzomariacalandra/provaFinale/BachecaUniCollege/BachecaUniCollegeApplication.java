a6b7ee77fefd33e202b91bd3f4b783d667bb2bd4
Calandra, Vincenzo Maria <vincenzomaria.calandra@nttdata.com>
2021-09-29 15:46:20 +0200

    Uploaded the Spring Boot Starter Folder

---
+++ BachecaUniCollege/src/main/java/com/vincenzomariacalandra/provaFinale/BachecaUniCollege/BachecaUniCollegeApplication.java
@@ -0,0 +1,13 @@
+package com.vincenzomariacalandra.provaFinale.BachecaUniCollege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachecaUniCollegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BachecaUniCollegeApplication.class, args);
+	}
+
+}
---
+++ BachecaUniCollege/src/test/java/com/vincenzomariacalandra/provaFinale/BachecaUniCollege/BachecaUniCollegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincenzomariacalandra.provaFinale.BachecaUniCollege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachecaUniCollegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
