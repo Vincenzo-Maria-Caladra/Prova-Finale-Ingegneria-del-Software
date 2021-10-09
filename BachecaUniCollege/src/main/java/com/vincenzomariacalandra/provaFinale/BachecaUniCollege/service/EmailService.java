606d16b2ebbfd2b9420f2a545ac186b077472719
Calandra, Vincenzo Maria <vincenzomaria.calandra@nttdata.com>
2021-10-09 17:06:27 +0200

    Implemented auth with token

---
+++ BachecaUniCollege/src/main/java/com/vincenzomariacalandra/provaFinale/BachecaUniCollege/service/EmailService.java
@@ -0,0 +1,57 @@
+package com.vincenzomariacalandra.provaFinale.BachecaUniCollege.service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import com.vincenzomariacalandra.provaFinale.BachecaUniCollege.utility.EmailSender;
+
+@Service
+public class EmailService implements EmailSender {
+	
+	
+	private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
+	
+	private final JavaMailSender javaMailsender;
+	
+	@Autowired
+	public EmailService(JavaMailSender javaMailsender) {
+		super();
+		this.javaMailsender = javaMailsender;
+	}
+
+
+
+	@Override
+	@Async
+	public void send(String to, String email) {
+		
+		try {
+			
+			MimeMessage mimeMessage = javaMailsender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+			
+			helper.setText(email, true);
+			helper.setTo(to);
+			helper.setSubject("Confirm your email");
+			helper.setFrom("hello@amigos.com");
+			
+			javaMailsender.send(mimeMessage);
+			
+		} catch (MessagingException e) {
+			
+			LOGGER.error("Failed to send the email", e);
+			
+			throw new IllegalStateException("Failed to send the email");		
+		}
+		
+	}
+
+}
