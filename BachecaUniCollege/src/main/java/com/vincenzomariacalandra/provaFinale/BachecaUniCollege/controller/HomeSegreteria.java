fbc3bc69822d1b611fbfc74e57d54b29c6619898
DESKTOP-QFEDLG8\Vinci <vincenzomaria.calandra@gmail.com>
2021-10-24 11:30:13 +0200

    Added Approvazione and Modifica Tertulie a Tema + minor changes

--- BachecaUniCollege/src/main/java/com/vincenzomariacalandra/provaFinale/BachecaUniCollege/controller/HomeSegreteria.java
+++ BachecaUniCollege/src/main/java/com/vincenzomariacalandra/provaFinale/BachecaUniCollege/controller/HomeSegreteria.java
@@ -1,11 +1,19 @@
 package com.vincenzomariacalandra.provaFinale.BachecaUniCollege.controller;
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.vincenzomariacalandra.provaFinale.BachecaUniCollege.model.Activity;
 import com.vincenzomariacalandra.provaFinale.BachecaUniCollege.service.ActivityService;
 import com.vincenzomariacalandra.provaFinale.BachecaUniCollege.service.UserActivityService;
 import com.vincenzomariacalandra.provaFinale.BachecaUniCollege.service.UserService;
@@ -27,11 +35,55 @@ public class HomeSegreteria {
 	}
 	
 	@GetMapping
-	public String getHomeSegreteria(Model model) {
+	public String getHomeSegreteria(@RequestParam(name = "id", required = false) Long id, Model model) {
 		
 		model.addAttribute("userCreditsList", userService.getAllUsersCredits());
 		model.addAttribute("tertulieList", activityService.getAllTertulieToBeApproved());
 		
+		if (id != null) {
+			model.addAttribute("form", Boolean.TRUE);
+			model.addAttribute("updatedActivity", new Activity());
+		} else {
+			model.addAttribute("form", Boolean.FALSE);
+
+		}
+		
 		return "homeSegreteria";
 	}
+	
+	
+	@RequestMapping(path = "/acceptTertulia", method = RequestMethod.POST)
+	public String acceptTertuliaATema (@RequestParam("id") Long id, Model model, HttpServletRequest request) {
+		
+		activityService.updateActivityState(id);
+		
+		return "redirect:/homeSegreteria";
+	}
+	
+	@RequestMapping(path = "/deleteTertulia", method = RequestMethod.POST )
+	public String deleteTertuliaATema (@RequestParam("id") Long id, Model model, HttpServletRequest request) {
+								
+		Optional<Activity> activityOptional = activityService.findActivityById(id);
+		
+		if (activityOptional.isPresent()) {
+			activityService.deleteActivity(activityOptional.get().getId());
+		}
+		
+		return "redirect:/homeSegreteria";
+	}
+	
+	
+	@RequestMapping(path = "/updateTertulia", method = RequestMethod.GET )
+	public String updateShowFormTertuliaATema (@RequestParam("id") Long id, Model model, HttpServletRequest request) {
+		
+		return "redirect:/homeSegreteria?id="+id+"#programmazioneTertulieATema";
+	}
+	
+	
+	@RequestMapping(path = "/updateTertulia", method = RequestMethod.POST )
+	public String updateTertuliaATema (@ModelAttribute Activity activity, @RequestParam("id") Long id, Model model, HttpServletRequest request) {
+		activityService.updateActivity(id, activity.getStartDate(), activity.getStartTime(), activity.getEndTime());
+		return "redirect:/homeSegreteria";
+	}
+	
 }
--- BachecaUniCollege/src/main/java/com/vincenzomariacalandra/provaFinale/BachecaUniCollege/service/ActivityService.java
+++ BachecaUniCollege/src/main/java/com/vincenzomariacalandra/provaFinale/BachecaUniCollege/service/ActivityService.java
@@ -2,12 +2,14 @@ package com.vincenzomariacalandra.provaFinale.BachecaUniCollege.service;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -125,6 +127,30 @@ public class ActivityService {
 		if (activityCredits != null &&
 				! Objects.equals(activityCredits, activity.getActivityCredits())) {
 			activity.setActivityCredits(activityCredits);
+		}
+		
+		return activity;
+	}
+	
+	
+	@Transactional
+	public Activity updateActivity(long activityId, Date startDate, Time startTime, Time endTime) {
+				
+		Activity activity = activityRepository.findById(activityId).orElseThrow( () -> new IllegalStateException(
+				"Activity with id:" + activityId + " not found!"));
+		
+		if(startDate.before(Date.valueOf(LocalDate.now()))) {
+			System.out.println(startDate);
+			System.out.println(Date.valueOf(LocalDate.now()));
+			throw new IllegalStateException("Data inserita antecedente ad oggi!");
+		} else {
+			
+			if (startTime.after(endTime)) {
+				throw new IllegalStateException("Ora di inizio successiva all'ora di fine!");
+			}
+			
+			activity.setStartDate(startDate);
+			activity.setStartTime(startTime.toString());
 		}
 		
 		return activity;
